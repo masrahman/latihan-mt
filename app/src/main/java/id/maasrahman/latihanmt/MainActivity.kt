abe011aef59d2899d712c20674657f5afee61a6f
Maas Rahman <cikok.maas@gmail.com>
2023-06-15 09:24:28 +0700

    sesi 3

--- app/src/main/java/id/maasrahman/latihanmt/MainActivity.kt
+++ app/src/main/java/id/maasrahman/latihanmt/MainActivity.kt
@@ -2,7 +2,10 @@ package id.maasrahman.latihanmt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import id.maasrahman.latihanmt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,22 +14,64 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         with(binding){
+            rbPria.setOnCheckedChangeListener(radioListener)
+            rbWanita.setOnCheckedChangeListener(radioListener)
+
+            cbAyam.setOnCheckedChangeListener(checkListener)
+            cbBakso.setOnCheckedChangeListener(checkListener)
+            cbRendang.setOnCheckedChangeListener(checkListener)
+
             btnSubmit.setOnClickListener {
                 if(etNama.text.toString().isEmpty()){
                     etNama.error = "Wajib diisi"
                     return@setOnClickListener
                 }
-                Toast.makeText(this@MainActivity,
-                    "Halo, ${etNama.text}",
-                    Toast.LENGTH_SHORT)
-                    .show()
+
+                var result = etNama.text.toString()
+                result += "\nStatus ${spnStatus.selectedItem}"
+                result += "\nJenis Kelamin ${if(rbPria.isChecked) "Pria" else "Wanita"}"
+                result += "\nMakanan Favorit ${favorite.joinToString(", ")}"
+
+                txtResult.text = result
             }
         }
     }
+    
+    val radioListener = CompoundButton.OnCheckedChangeListener{ button, isChekced ->
+        if(isChekced){
+            if(button.id == R.id.rbPria){
+                binding.rbWanita.isChecked = false
+            }else if(button.id == R.id.rbWanita){
+                binding.rbPria.isChecked = false
+            }
+        }
+    }
+
+
+    var favorite = arrayListOf<String>()
+
+    val checkListener = CompoundButton.OnCheckedChangeListener { button, isChecked ->
+        if(isChecked){
+            favorite.add(button.text.toString())
+        }else{
+            favorite.remove(button.text.toString())
+        }
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 }
